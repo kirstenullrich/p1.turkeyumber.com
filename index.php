3729f19a8bb59f8fb0f207c750cd85a0a8d1936b
Kirsten Ullrich <kirstenullrich@gmail.com>
2013-09-15 22:12:55 -0400

    First draft with hipster ipsum placeholder text

--- index.php
+++ index.php
@@ -1,1 +1,42 @@
-Welcome to P1
+<!DOCTYPE html>
+<html lang="en">
+
+
+<head>
+
+	<title>P1</title>
+	<link href="style.css" rel="stylesheet">
+	<link href='http://fonts.googleapis.com/css?family=Raleway:400,800' rel='stylesheet' type='text/css'>
+
+</head>
+
+
+<body>
+
+	<div class="container">
+
+		<div class="infobox">
+
+			<h1>Kirsten Ullrich</h1>
+			<h2>Turkey Umber</h2>
+				<p>
+					Pariatur kitsch locavore Brooklyn. Eiusmod Neutra placeat distillery, ethical narwhal quis. Photo booth Pinterest delectus roof party, VHS keytar ugh bitters ea Pitchfork. Chambray street art deep v odio ethical, semiotics esse raw denim cray. Fanny pack deep v squid Terry Richardson, polaroid synth do iPhone freegan brunch. Biodiesel proident scenester before they sold out cardigan beard, plaid labore Vice raw denim. Meh you probably haven't heard of them farm-to-table pop-up, Brooklyn keytar food truck PBR&B ugh anim pug scenester mustache kogi cardigan.
+				</p>
+
+			<h2>The Rest</h2>
+				<p>
+	Literally aliqua master cleanse, lomo retro laboris High Life single-origin coffee actually butcher polaroid magna irure next level nesciunt. Etsy aliqua freegan food truck commodo, gentrify Bushwick adipisicing tempor. Mumblecore 90's cupidatat Tonx jean shorts. Eiusmod aliqua jean shorts, skateboard est 8-bit sed actually stumptown forage nihil. Fixie ethnic YOLO, elit yr placeat consectetur banh mi Odd Future Wes Anderson commodo. Dolor ethical Intelligentsia sapiente. Cardigan meh Schlitz Pinterest magna, proident asymmetrical quis post-ironic consequat culpa ullamco.
+				</p>
+
+			<h2>Code Editor</h2>
+				<p>Sublime Text 2</p>
+
+		</div> <!--close infobox-->
+
+	</div><!--close container-->
+
+</body>
+
+
+</html>
+
