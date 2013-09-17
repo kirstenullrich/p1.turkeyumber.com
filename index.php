308d9c11fb10f4d3a6c6327898f594f165ed79d8
Kirsten Ullrich <kirstenullrich@gmail.com>
2013-09-16 23:46:24 -0400

    Final tweaks to the project

--- index.php
+++ index.php
@@ -19,19 +19,37 @@
 		<div class="infobox">
 
 			<h1>Kirsten Ullrich, Project 1</h1>
+			
 			<h2>About</h2>
 				<p>
-					I'm a distance student originally from Louisville, Kentucky who has lived in a good handful of places before landing in Brattleboro, Vermont with husband and cat five years ago. I earn my living doing graphic design, mostly for print, but increasing demand for web design has led me to explore 'the other side.' What I've learned thus far about web design and coding is a flea market-worthy hodgepodge of things that derive from using Dreamweaver, watching videos on lynda.com and treehouse.com, and (most informatively) from tackling projects that were bigger than my knowledge base at the time. As a result, I have a hard-earned working knowledge of web design that has plenty of gaps. And I also discovered that I really enjoy the process and challenge of figuring out a code problem and making it work. Who knew?.
+					I'm a distance student originally from Louisville, Kentucky who has lived in a good handful of places before landing in Brattleboro, Vermont with husband and cat five years ago. 
+					I earn my living doing graphic design, mostly for print, but increasing demand for web design has led me to explore 'the other side' of design. 
+					What I've learned thus far about web design and coding is a flea market hodgepodge of things, most of which I gleaned by tackling projects that were bigger than my knowledge base at the time. 
+					As a result, I have a hard-earned working knowledge of web design that has plenty of gaps. What I also discovered by doing is that I really enjoy the process and challenge of figuring out a code problem and making it work. Who knew?
 				</p>
 
 			<h2>Turkey Umber</h2>
 				<p>
-The part of my life that isn't devoted to making a living is devoted to making art. I got my MFA from the Tyler School of Art in 2005; I'm a painter and sculptor. While I use nearly any material I can get my hands on, I have an enduring love of oil paint, in part because of its incredible variety of very specific colors with long histories. One of those colors is turkey umber; the pigment is great, but I love it just as much for having a name that evokes Istanbul and a big, goofy bird at the same time. And, yes, I actually do use quite a bit of it. Here's what turkey umber looks like:
+					The part of my life that isn't devoted to making a living is devoted to making art. 
+					I got my MFA from the Tyler School of Art in 2005; I'm a painter and sculptor. 
+					While I use nearly any material I can get my hands on, I have an enduring love of oil paint, in part because of its incredible variety of very specific colors with long histories. 
+					One of those colors is turkey umber; the pigment is great, but I love it just as much for having a name that simultaneously evokes Istanbul and a big, goofy bird. 
+					And, yes, I actually do use quite a bit of it. Here's what turkey umber looks like:
 				</p>
 
-			<img src="images/turkeyumberswatch.jpg" class='space' alt="swatch of turkey umber color"/>
+			<a href="http://books.google.com/books?id=RJnI4ld6qeoC&pg=PA144&lpg=PA144&dq=turkey+umber+pigment&source=bl&ots=9TP_ZL7oRz&sig=-blmaKbyiRDziHrSNrSNxQzQDVw&hl=en&sa=X&ei=4xUzUta0PPHF4APPy4H4Dw&ved=0CHYQ6AEwBzgK#v=onepage&q=turkey%20umber%20pigment&f=false" target="_blank"
+			title="'The name umber is derived from the Latin ombra (shadow) and describes simply the colour of a pigment ... Geologically, umbers are very fine-grained sedimentary rocks composed of manganese hydroxides and oxides (primarily the minerals manganite and pyrolusite) plus iron hydroxide (goethite) particles.
+
+The Cyprus-type umbers, with their olive-greenish cast, produce by far the best pigment and particularly popular was that derived from the Margi area, west of Nicosia.
+
+Cyprus umber is often referred to as Turkey umber or Turkish umber.' 
+
+- from pages 144 and 383 of Pigment Compendium: A Dictionary and Optical Microscopy of Historical Pigments, by Valentine Walsh and Tracey Chaplin, published by Routledge, 2008 ">
+				<img src="images/turkeyumberswatch.jpg" class='space' alt="swatch of turkey umber color"/>
+			</a>
+
 				<p>
-My work is online at <a href="http://www.kirstenullrich.com">kirstenullrich.com</a>.
+					My artwork is online at <a href="http://www.kirstenullrich.com" target="_blank">kirstenullrich.com</a>.
 				</p>
 
 
